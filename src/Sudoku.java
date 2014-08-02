3d047893ef848a113bdf66baf783e1ecf482a1cf
Geir KH <geir.kihle.hanssen@gmail.com>
2014-08-03 00:28:32 +0200

    Revert of changes

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,10 +1,13 @@
+import javax.swing.*;
+
 /**
  * Created by Teioch on 01.08.2014.
  */
 public class Sudoku {
+
     public static void main(String[] args){
         Board board = new Board();
         board.printBoard();
-        System.out.println("TEST");
+
     }
 }
