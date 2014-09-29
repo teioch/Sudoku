4aba5eb85721bf1e55d97bcf6cb1a31831de10fa
Geir KH <geir.kihle.hanssen@gmail.com>
2014-09-29 14:56:29 +0200

    Added rule checks

--- src/Board.java
+++ src/Board.java
@@ -10,7 +10,7 @@ public class Board {
         board = new int[width][height];
         for(int i = 0; i < 9; i++){
             for(int k = 0; k < 9; k++){
-                board[i][k] = k+1;
+                board[i][k] = 0;
             }
         }
     }
@@ -25,5 +25,37 @@ public class Board {
             System.out.println("-------------------------------------");
 
         }
+    }
+
+    public boolean intExistsHorizontally(int nr, int col){
+        for(int i = 0; i < height; i++){
+            if(board[i][col] == nr){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean intExistsVertically(int nr, int row){
+        for(int i = 0; i < width; i++){
+            if(board[row][i] == nr){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean intExistsInBox(int nr, int row, int col){
+        int rowBox = (row-1 / 3) * 3;
+        int colBox = (col-1 / 3) * 3;
+
+        for(int i = rowBox; i < rowBox + 3; i++){
+            for(int k = colBox; k < colBox + 3; k++){
+                if(board[i][k] == nr){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
